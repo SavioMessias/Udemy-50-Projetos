a49c762cf32af871276a4f0a7601e330343ab7d9
Savio <kiqsavio@gmail.com>
2023-07-12 22:01:55 -0300

    reinitialized files

---
+++ Pokedex/script.js
@@ -0,0 +1,63 @@
+const poke_container = document.getElementById('poke-container')
+const pokemon_count = 150
+
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#F4E7DA',
+    rock: '#D5D5D4',
+    fairy: '#98D7A5',
+    bug: '#F8D5A3',
+    dragon: '#97B3E6',
+    psychic: '#EAEDA1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5',
+}
+
+const main_types = Object.keys(colors)
+
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+    const res = await fetch(url)
+    const data = await res.json()
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    
+    console.log(pokemon)
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+    </div>
+        <div class="info">
+            <span class="number">#${id}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span></span> </small>
+        </div>
+    </div>
+    `
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    poke_container.appendChild(pokemonEl)
+
+    
+}
+
+fetchPokemons()
